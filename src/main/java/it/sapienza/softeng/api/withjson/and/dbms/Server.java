ed907781708514c1395d141a361e689aa8472e2c
giacampeti <98210231+giacampeti@users.noreply.github.com>
2022-01-22 11:06:25 +0100

    Update Server.java

--- src/main/java/it/sapienza/softeng/api/withjson/and/dbms/Server.java
+++ src/main/java/it/sapienza/softeng/api/withjson/and/dbms/Server.java
@@ -28,7 +28,7 @@ public class Server {
         factoryBean.setResourceProvider(new SingletonResourceProvider(wr));
 
         /* set address to host */
-        factoryBean.setAddress("https://pipboy2022.herokuapp.com/");
+        factoryBean.setAddress("https://localhost:8080/");
 
         /* set providers */
         List<Object> providers = new ArrayList<Object>();
